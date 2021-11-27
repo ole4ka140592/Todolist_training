099ef73024c52fe1a6534a376fb094eda05df914
ole4ka140592 <ole4ka140592@gmail.com>
2021-11-27 23:39:02 +0300

    add store

---
+++ src/state/store.ts
@@ -0,0 +1,18 @@
+import {tasksReducer} from './taskReducer';
+import {todolistsReducer} from './todolistsReducer';
+import {combineReducers, createStore} from "redux";
+
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+
+
+export const store = createStore(rootReducer);
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+
+// @ts-ignore
+window.store = store;
