64ff5d761331df6fab327caee201b61bf7926669
ole4ka140592 <ole4ka140592@gmail.com>
2021-12-03 23:29:41 +0300

    add Task.STORIES

---
+++ src/components/Task.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {AddItemForm} from "./AddItemForm";
+import {action} from "@storybook/addon-actions";
+import {Task} from "./Task";
+import {TaskPropsType} from "../Todolist";
+
+
+export default {
+    title: 'Todolist/Task',
+    component: Task,
+    args: {
+        removeTask: action("removeTask"),
+        changeStatus: action("changeStatus"),
+        updateTask: action("updateTask")
+    }
+} as ComponentMeta<typeof Task>;
+
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+
+export const TaskIsDoneStory = Template.bind({});
+TaskIsDoneStory.args = {
+    task: {id: "123", title: "React", isDone: true},
+};
+
+export const TaskNotDoneStory = Template.bind({});
+TaskNotDoneStory.args = {
+    task: {id: "123", title: "React", isDone: false},
+
+};
+
+
+
+
+
