08dd503b79b6817fb668f739369fea23d4842218
ole4ka140592 <ole4ka140592@gmail.com>
2021-12-04 02:52:21 +0300

    add decorators

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -5,17 +5,17 @@ import {action} from "@storybook/addon-actions";
 import {AppWithRedux} from "./AppWithRedux";
 import {store} from "./state/store";
 import {Provider} from "react-redux";
+import {ReduxStoreProviderDecorator} from "./state/decorators/ReduxStoreProviderDecorator";
 
 
 export default {
     title: 'Todolist/AppWithRedux',
     component: AppWithRedux,
+    decorators: [ReduxStoreProviderDecorator]
 } as ComponentMeta<typeof AppWithRedux>;
 
-const Template: ComponentStory<typeof AppWithRedux> = (args) =>
-    <Provider store={store}>
-        <AppWithRedux/>
-    </Provider>;
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux/>
+
 
 export const AppWithReduxStory = Template.bind({});
 
