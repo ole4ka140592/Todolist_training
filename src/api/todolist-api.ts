44057b3a62c36bd4576660f83900e6369b1bba13
ole4ka140592 <ole4ka140592@gmail.com>
2022-05-02 00:06:58 +0300

    changes api-key

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '5f364300-3de1-45b6-9da5-36144'
+        'API-KEY': '5f364300-3de1-45b6-9da5-36144badd296'
     }
 }
 
